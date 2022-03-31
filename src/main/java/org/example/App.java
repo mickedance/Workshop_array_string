ab5fbf02200b3bdd94b5cddc0b1ac9c6a0d93db8
mickedance <mickedance@hotmail.com>
2022-03-31 09:47:02 +0200

    Part1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,25 @@
 package org.example;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        // Se size of empty Array
+        System.out.println(NameRepository.getSize());
+
+        // Add names to array
+        String[] names = {"Mikael Svensson", "klara Sagasson"};
+        NameRepository.setNames(names);
+
+        // See current size
+        System.out.println(NameRepository.getSize());
+
+        //Print out all names in array
+        System.out.println(Arrays.toString(NameRepository.findAll()));
+
+
     }
 }
--- src/main/java/org/example/NameRepository.java
+++ src/main/java/org/example/NameRepository.java
@@ -3,7 +3,7 @@ package org.example;
 import java.util.Arrays;
 
 public class NameRepository {
-    private static String[] string;
+    private static String[] names = new String[0];
 
     public static void main(String[] args) {
 
@@ -12,24 +12,46 @@ public class NameRepository {
 
     //     Returns number of elements in the array
     public static int getSize() {
-        return string.length;
+        return names.length;
     }
 
     /*Sends in an array that set the private static array.
     This should replace all existing names.*/
-    public static void setNames(String[] names) {
+    public static void setNames(String[] _names) {
+        clear();
+        for (String name : _names) {
 
+            if (name.split(" ").length == 2) {
+                String nameFirst = name.split(" ")[0];
+                String nameLast = name.split(" ")[1];
+                if (nameFirst.length() > 0 && nameLast.length() > 0) {
+                    if (addName(nameFirst, nameLast))
+                        System.out.println("Name: " + names[names.length - 1] + " Added");
+                    else
+                        System.out.println("Error on setting new names");
+                }
+            } else {
+                System.out.println("Wrong namingformat. ");
+            }
+        }
+    }
+
+    private static boolean addName(String name1, String name2) {
+        names = Arrays.copyOf(names, names.length + 1);
+        names[names.length - 1] = name1.trim() + " " + name2.trim();
+
+        return names[names.length - 1].equals(name1 + " " + name2);
     }
 
     //     Should completely empty the array.
     public static void clear() {
-        string = new String[0];
+        names = new String[0];
+        System.out.println("Array name cleared!");
     }
 
     //    Returns all names in a new array
     public static String[] findAll() {
-
-        return string;
+        return names;
 
     }
 }
